69a8967526064ab5cbf9febeb0cb04446985f2b0
MikaAK <me@mikakalathil.ca>
2016-03-31 23:41:39 -0700

    Add moment formatting

--- src/app/Sprints/index.ts
+++ src/app/Sprints/index.ts
@@ -4,12 +4,15 @@ import {RouterLink, Router} from 'angular2/router'
 import {BackButton} from 'shared/directives/BackButton'
 import {Sprints} from 'shared/services/Sprints'
 
+import {MomentPipe} from 'shared/pipes/MomentPipe'
+
 @Component({
   selector: 'sprints',
   template: require('./Sprints.jade')(),
   styles: [require('./Sprints.scss')],
   directives: [BackButton, RouterLink],
-  providers: [Sprints]
+  providers: [Sprints],
+  pipes: [MomentPipe]
 })
 export class SprintsComponent {
   constructor(public router: Router, public sprints: Sprints) {
---
+++ src/shared/pipes/MomentPipe.ts
@@ -0,0 +1,19 @@
+import * as moment from 'moment'
+import {Moment} from 'moment'
+import {Pipe, PipeTransform} from 'angular2/core'
+
+/*
+ * Raise the value exponentially
+ * Takes an exponent argument that defaults to 1.
+ * Usage:
+ *   value | exponentialStrength:exponent
+ * Example:
+ *   {{ date |  momentDate: "MMMM-DDD"}}
+ *   formats to: January-80
+*/
+@Pipe({name: 'momentDate'})
+export class MomentPipe implements PipeTransform {
+  transform(value: Moment, [format]): string {
+    return moment(value).format(format || 'dddd MMMM Do')
+  }
+}
